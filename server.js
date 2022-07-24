133af91c024f5050b4353cdcd1f166841aefe559
Aleksa Radosavljevic <aleksa.radosavljevic@lakesidernd.com>
2022-07-24 15:23:15 +0200

    update path to db.json file

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.listen(appPort);
 
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('src/db/db.json');
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({ static: './dist/spa' });
 const port = process.env.PORT || 3000;
 
