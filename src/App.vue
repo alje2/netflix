2ec97fdf8cedc90e904a9f519b5ca4c230bf9e33
Aleksa Radosavljevic <radosavljevic.aleksa2@gmail.com>
2022-11-12 00:09:51 +0100

    feat: custom icon set quasar components using material icons

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,24 @@
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { useQuasar } from 'quasar';
+import { defineComponent } from 'vue';
+import customIconSet from 'src/icons/custom-icons.js';
 export default defineComponent({
-  name: 'App'
-})
+  name: 'App',
+  setup() {
+    const $q = useQuasar();
+
+    function changeIconSetToCustomIconSet() {
+      $q.iconSet.set(customIconSet);
+    }
+
+    return {
+      changeIconSetToCustomIconSet,
+    };
+  },
+  beforeCreate() {
+    this.changeIconSetToCustomIconSet();
+  },
+});
 </script>
